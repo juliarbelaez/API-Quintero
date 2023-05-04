96fba200d1a95e5c1442facadd18fa7808494ed2
Juliana Quintero <juli.carmesi@gmail.com>
2023-05-03 21:47:05 -0500

    "API changes"

--- src/app/admin/pages/inscripciones/inscripciones/inscripciones.module.ts
+++ src/app/admin/pages/inscripciones/inscripciones/inscripciones.module.ts
@@ -1,33 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InscripcionesComponent } from './inscripciones.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatDialogModule } from '@angular/material/dialog';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSelectModule } from '@angular/material/select';
-import { FormsModule } from '@angular/forms';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatButtonModule } from '@angular/material/button';
-import { PipesModule } from 'src/app/share/pipes/pipes.module';
+
 @NgModule({
   declarations: [InscripcionesComponent],
-  imports: [
-    CommonModule,
-    MatFormFieldModule,
-    MatDialogModule,
-    ReactiveFormsModule,
-    MatInputModule,
-    MatIconModule,
-    MatSelectModule,
-    FormsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatButtonModule,
-    PipesModule,
-  ],
+  imports: [CommonModule],
   exports: [InscripcionesComponent],
 })
 export class InscripcionesModule {}
